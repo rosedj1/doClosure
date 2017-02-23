fb23fa7e8ef12a5213c0652eb7652607c60ee759
mhl0116 <mhl0116>
2017-02-23 09:05:05 -0500

    data correction for 2016 muon done

--- ZClosure/doAllClosure_mZ.py
+++ ZClosure/doAllClosure_mZ.py
@@ -28,10 +28,10 @@ def doAllClosure(fs, plotPath, Z_width, plotBinInfo, singleCB_tail, pTErrCorrect
 #    inputpath = '/raid/raid9/mhl/HZZ4L_Run2_post2016ICHEP/outputRoot/Data_2016_v1/'
 #    inputpath = '/raid/raid9/mhl/HZZ4L_Run2/HZZ4L/PereventMassErrCorr_2016ICHEP/PereventMassErrCorr_v0_forData/'
 #    inputpath = '/raid/raid9/mhl/HZZ4L_Run2/HZZ4L/PereventMassErrCorr_2016ICHEP/getCorrection_ICHEP2016/inputRoot/'
-    filename = 'DYJetsToLL_M-50_kalman_v4_m'+fs+'.root'
+#    filename = 'DYJetsToLL_M-50_kalman_v4_m'+fs+'.root'
 #    inputpath = '/raid/raid9/mhl/HZZ4L_Run2_post2016ICHEP/outputRoot/DY_2015MC_kalman_v4_76X/'
-    inputpath = '/raid/raid9/mhl/HZZ4L_Run2_post2016ICHEP/outputRoot/DY_2016MC_v1_20170222/'
-#    filename = 'DoubleLepton_m'+fs+'.root'
+    inputpath = '/raid/raid9/mhl/HZZ4L_Run2_post2016ICHEP/outputRoot/Data_2016_v1_20170223/'
+    filename = 'DoubleLepton_m'+fs+'.root'
     outtxtName = '../makeSummaryPlots/sigma_m'+fs+'.txt'
 
     call('echo " " > ' + outtxtName, shell=True)
@@ -54,7 +54,7 @@ def doAllClosure(fs, plotPath, Z_width, plotBinInfo, singleCB_tail, pTErrCorrect
 
 #plotBinInfo = '100 80 100'
 plotBinInfo = '300 60 120'
-plotpath = '/home/mhl/public_html/2017/20170222_muCorr/fitmassZ/'
+plotpath = '/home/mhl/public_html/2017/20170223_lepCorr_data/fitmassZ/'
 
 singleCB_tail_mu = '1.583 1.086' #first is alpha, second is n of singleCB
 singleCB_tail_e = '1.1443 2.5964' #first is alpha, second is n of singleCB
@@ -64,7 +64,7 @@ pTErrCorrections_mu = '1.251 1.292 1.117 1'
 pTErrCorrections_e = '1.245 1.140 1.077 1.178'
 
 #ZWidth = 2.37
-ZWidth = 2.44
+ZWidth = 2.49
 
 doAllClosure('2mu', plotpath, ZWidth, plotBinInfo, singleCB_tail_mu, pTErrCorrections_mu)
 #doAllClosure('2e', plotpath, ZWidth, plotBinInfo, singleCB_tail_e, pTErrCorrections_e)
