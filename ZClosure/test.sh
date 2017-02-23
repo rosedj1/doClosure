0b50299b84988b4e70aab8b204478b13e495001e
mhl0116 <mhl0116>
2017-02-23 04:06:35 -0500

    done @ 20170222

--- ZClosure/test.sh
+++ ZClosure/test.sh
@@ -1,8 +1,8 @@
-plotpath="/home/mhl/public_html/2017/20170222_muCorr/fitmassZ/"
+plotpath="/home/mhl/public_html/2017/20170222_eCorr/fitmassZ/"
 
 cd ../makeSummaryPlots;
-#./makeGraph.sh 2e $plotpath
-./makeGraph.sh 2mu $plotpath
+./makeGraph.sh 2e $plotpath
+#./makeGraph.sh 2mu $plotpath
 
 #./makeGraph.sh 4mu_reco $plotpath
 #./makeGraph.sh 4mu_refit $plotpath
